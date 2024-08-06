2ca6b0277d832898209cd2ea32e3659ff884df30
Package <2778120659@qq.com>
2024-08-06 13:47:19 +0800

    找回文件

--- Sort/QuickSort.c
+++ Sort/QuickSort.c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include"Sort.h"
 void quicksort(int* arr, int begin, int end)
 {
@@ -68,4 +69,75 @@ int partsort2(int* arr,int begin,int end)
 		swap(&arr[prev], arr[key]);
 		return key;
 	}
+=======
+#include"Sort.h"
+void quicksort(int* arr, int begin, int end)
+{
+	if (begin >= end)
+	{
+		return;
+	}
+	int key = partsort_double_pointer(arr,begin,end);
+	/*while (left < right)
+	{
+		while (left<right&&arr[right]>=arr[key])
+		{
+			right--;
+		}
+		while (left<right&&arr[left]<=arr[key])
+		{
+			left++;
+		}
+		swap(&arr[left], &arr[right]);
+	}
+	swap(&arr[right], &arr[key]);*/
+
+	
+	quicksort(arr, begin, key - 1);
+	quicksort(arr, key + 1, end);
+}
+int partsort(int* arr, int begin, int end)
+{
+	int left = begin;
+	int right = end;
+	int hole = left;
+	while (left < right)
+	{
+		while (left<right && arr[right]>=arr[hole])
+		{
+			right--;
+		}
+		arr[hole] = arr[right];
+		hole = right;
+		while (left < right && arr[left] <= arr[hole])
+		{
+			left++;
+		}
+		arr[hole] = arr[left];
+		hole = left;
+	}
+	return hole;
+}
+int partsort_double_pointer(int* arr, int begin,int end)
+{
+	int prev = begin;
+	int pcur = prev + 1;
+	int key = begin;
+	while (pcur <= end)
+	{
+		if (arr[pcur] < arr[key])
+		{
+			prev++;
+			swap(&arr[prev], &arr[pcur]);
+			pcur++;
+		}
+		else
+		{
+			pcur++;
+		}
+
+	}
+	swap(&arr[prev], &arr[key]);
+	return key;
+>>>>>>> Stashed changes
 }
